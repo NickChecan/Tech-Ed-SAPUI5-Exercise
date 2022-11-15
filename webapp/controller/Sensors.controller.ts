d8992119ad83d15af29705a6ccce952d37e2070f
Nicholas Checan <nicholas.checan@gmail.com>
2022-11-15 20:47:54 +0000

    Fragment Containing a SelectDialog

--- webapp/controller/Sensors.controller.ts
+++ webapp/controller/Sensors.controller.ts
@@ -7,6 +7,9 @@ import { IconColor } from "sap/ui/core/library";
 import Filter from "sap/ui/model/Filter";
 import Event from "sap/ui/base/Event";
 import ListBinding from "sap/ui/model/ListBinding";
+import SelectDialog from "sap/m/SelectDialog";
+import Control from "sap/ui/core/Control";
+import Fragment from "sap/ui/core/Fragment";
 
 enum Threshold {
     Warm = 4,
@@ -20,11 +23,11 @@ export default class Sensors extends Controller {
 
     public onInit(): void {
         const ownerComp = this.getOwnerComponent();
-        this.getSensorModel().dataLoaded().then(function() {
+        this.getSensorModel().dataLoaded().then(function () {
             const resourceModel = (ownerComp?.getModel("i18n") as ResourceModel);
             const resourceBundle = (resourceModel.getResourceBundle() as ResourceBundle);
             MessageToast.show(resourceBundle.getText("msgSensorDataLoaded"), { closeOnBrowserNavigation: false });
-        }.bind(this)).catch(function(oErr: Error){
+        }.bind(this)).catch(function (oErr: Error) {
             MessageToast.show(oErr.message, { closeOnBrowserNavigation: false });
         });
     }
@@ -35,7 +38,7 @@ export default class Sensors extends Controller {
         return model;
     }
 
-    formatIconColor(temperature: number): IconColor|string {
+    formatIconColor(temperature: number): IconColor | string {
         if (temperature < Threshold.Warm) {
             return "#0984e3";
         } else if (temperature >= Threshold.Warm && temperature < Threshold.Hot) {
@@ -63,8 +66,52 @@ export default class Sensors extends Controller {
             this.statusFilters = [];
         }
 
-        listBinding.filter(this.statusFilters);
-        
+        listBinding.filter(this.statusFilters.concat(this.customFilters));
+
     }
-    
+
+    private dialog: Promise<SelectDialog>;
+
+    onCustomerSelect(): void {
+        if (!(this.dialog instanceof Promise)) {
+
+            const sensorModel = this.getSensorModel();
+            const resourceModel = this.getView()?.getModel("i18n") as ResourceModel;
+
+            this.dialog = Fragment.load({
+                type: "XML",
+                name: "keepcool.sensormanager.view.CustomerSelectDialog",
+                controller: this
+            }).then(function (control: Control | Control[]) {
+                const dialog = (control instanceof Array ? control[0] : control) as SelectDialog;
+                dialog.setModel(sensorModel, "sensorModel");
+                dialog.setModel(resourceModel, "i18n");
+                dialog.setMultiSelect(true);
+                return dialog;
+            });
+        }
+
+        this.dialog.then(function (dialog) {
+            dialog.open("");
+        }).catch(function (err: Error) {
+            MessageToast.show(err.message);
+        });
+    }
+
+    onCustomerSelectChange(event: Event): void {
+        const value = (event.getParameter("value") as string);
+        const filter = new Filter("name", "Contains", value);
+        const listBinding = (event.getSource() as Control).getBinding("items") as ListBinding;
+        listBinding.filter([filter]);
+    }
+
+    onCustomerSelectConfirm(event: Event): void {
+        const selectedItems = (event.getParameter("selectedItems") as StandardListItem[]);
+        const listBinding = this.getView()?.byId("sensorsList")?.getBinding("items") as ListBinding;
+        this.customFilters = selectedItems.map(function(item: StandardListItem) {
+            return new Filter("customer", "EQ", item.getTitle());
+        });
+        listBinding.filter(this.customFilters.concat(this.statusFilters));
+    }
+
 }
